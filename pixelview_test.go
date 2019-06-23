711363859c67b62faa434fc137c96164e9171894
Joey Sabey <GameFreak7744@gmail.com>
2019-06-23 21:10:41 +0100

    Removed pointless TestFromFile() test

--- pixelview_test.go
+++ pixelview_test.go
@@ -14,31 +14,6 @@ import (
 var update = flag.Bool("update", false, "update .golden files")
 
 
-func TestFromFile(t *testing.T) {
-    buf, err := ioutil.ReadFile("pixelview.raw")
-    if err != nil {
-        panic(err)
-    }
-    reference := string(buf)
-
-    s, err := FromFile("pixelview.png")
-    if err != nil {
-        t.Errorf("Error encountered during execution: %s", err)
-    }
-    if s != reference {
-        t.Error("Output did not match reference")
-    }
-
-    s, err = FromFile("pixelview_uneven.png")
-    if err == nil {
-        t.Error("Allowed uneven height image without error")
-    }
-    if s != "" {
-        t.Errorf("Returned non-zero result when given uneven image: %s", s)
-    }
-}
-
-
 func TestFromImageGeneric(t *testing.T) {
     golden := filepath.Join("testdata", t.Name()+".golden")
     buf, err := ioutil.ReadFile(golden)
